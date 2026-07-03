6b91c5e9418470a22d88b0b3488b67a2c6e509b3
Jay Raj <haldarjayraj@gmail.com>
2026-07-03 23:22:31 +0530

    Initializing pydantic course

---
+++ 1_pydantic_why.py
@@ -0,0 +1,8 @@
+#we're recieving patient's data and inserting it in our database
+def insert_patient_data(name, age):
+
+    print(name)
+    print(age)
+    print('inserted into database')
+    #koi junior programmer iss function ko use karega in order to insert data into the database
+    #python me dynamic typing hoti hai static nahi like java or C++
